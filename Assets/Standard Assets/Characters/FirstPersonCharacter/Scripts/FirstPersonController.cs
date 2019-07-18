7b649880d215e16830a6a5f203f12d5433bd27b7
bli0428 <brandon_li@brown.edu>
2019-07-18 10:03:50 -0400

    Added new plane for Scene

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -236,7 +236,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void RotateView()
         {
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
+            // m_MouseLook.LookRotation (transform, m_Camera.transform);
         }
 
 
