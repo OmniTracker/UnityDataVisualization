8dfaf8bf9bac5790dcc26e148eba206da6dbb7e4
Omnitracker <ronald_baker@brown.edu>
2019-07-24 00:06:09 -0400

    Data Point Resizing Done in VR Scene

--- Assets/RW/Scripts/Raycaster.cs
+++ Assets/RW/Scripts/Raycaster.cs
@@ -11,6 +11,9 @@
  *      Revision History:
  *      
  *      (7/11/2019) - Software Refactor - This file was generated to handle the
+ *                                        Raycast of trigger handle
+ *      (7/23/2019) - Software Refactor - Adjusted Raycast to handle GUI similar
+ *                                        to standalone.
  *-----------------------------------------------------------------------------
  * This program is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
@@ -22,8 +25,6 @@
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  */
 
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 using UnityEngine.UI;
@@ -32,195 +33,46 @@ public class Raycaster : MonoBehaviour
 {
     public SteamVR_Input_Sources LeftInputSource = SteamVR_Input_Sources.LeftHand;
     public SteamVR_Input_Sources RightInputSource = SteamVR_Input_Sources.RightHand;
-    private Dropdown m_dropdown = null;
-    // The prefab for the data Magnet that will be instantiated
-    public GameObject MagnetHolder;
-    public GameObject PointHolder;
-    // Public Dropdown menus
-    public Dropdown XCoordinateDropDown;
-    public Dropdown YCoordinateDropDown;
-    public Dropdown ZCoordinateDropDown;
-    public Dropdown MagnetNameDropDown;
-    private int m_currentMagnetNameDropDownIndex = -1;
-    // Public
-    public Text MagnetStrengthTextValue;
-    public Text SpringStrengthTextValue;
-    // Public
-    public Text MagnetDataMenu;
-    public Text PointDataMenu;
-    // Public Toggle Objects
-    public Toggle MagnetActiveToggle;
-    public Toggle AllowMagnetismToggle;
-    public Toggle AllowSpringToggle;
-    // Private Magnetism Value
-    private int m_MinimumMagnetValue = -100;
-    private int m_MaximumMagnetValue = 100;
-    // Private Spring Strength
-    private int m_MinimumSpringValue = 0;
-    private int m_MaximumSpringValue = 50;
-    // Used to make sure that all the child Magnets are properly counted
-    private int m_ChildMagnetCount = 0;
-    private string m_FileName;
-    private List<string> m_magnetNameList;
+
+    string m_lastPointDataName = "";
     /// <summary>
     /// 
     /// </summary>
     void Start()
     {
-        InvokeRepeating("UpdateRaycaster", 2.0f, 0.1f);
-        // Set the text for the Magnetic
-        MagnetStrengthTextValue.text = "0";
-        SpringStrengthTextValue.text = "25";
-        m_magnetNameList = new List<string>();
+        InvokeRepeating("UpdateRaycaster", 2.0f, 0.2f);
     }
     private void UpdateRaycaster()
     {
         RaycastHit raycastHit;
         GameObject gameObject = null;
-
-        if( HasDataFileBeenUpdated() )
-        {
-            // Clear current Magnet Name List is are currently names stored
-            if (m_magnetNameList.Count > 0)
-                m_magnetNameList.Clear();
-            // Fill the magnetList
-            foreach (Transform childMagnet in MagnetHolder.transform)
-                m_magnetNameList.Add(childMagnet.name);
-            // Update Dropdowns
-            UpdateDropDownList();
-            // Set the current plot positions for the plot 
-            SetXYZDropdownsToCurrentPlotAxis();
-        }
-
-        if (m_currentMagnetNameDropDownIndex != MagnetNameDropDown.value)
-        {
-            MagnetAttributes attributes = GameObject.Find(MagnetNameDropDown.options[MagnetNameDropDown.value].text).GetComponent<MagnetAttributes>();
-            MagnetActiveToggle.isOn = attributes.MagnetActive;
-            AllowMagnetismToggle.isOn = attributes.MagnetVisible;
-            MagnetStrengthTextValue.text = attributes.MagnetismStrength.ToString();
-            m_currentMagnetNameDropDownIndex = MagnetNameDropDown.value;
-        }
-
-        if (SteamVR_Actions._default.Squeeze.GetAxis(LeftInputSource) == 1 || 
-            SteamVR_Actions._default.Squeeze.GetAxis(RightInputSource) == 1)
+        if (SteamVR_Actions._default.Squeeze.GetAxis(LeftInputSource) == 1 || SteamVR_Actions._default.Squeeze.GetAxis(RightInputSource) == 1)
         {
             if (Physics.Raycast(transform.position, transform.forward, out raycastHit))
             {
                 gameObject = raycastHit.collider.gameObject;
+
                 if (gameObject.tag.Contains("RaycasterUI"))
                 {
                     InteractWithGUI(gameObject);
                 }
-                else if (gameObject.tag.Contains("Magnet") || gameObject.tag.Contains("DataPoint"))
+                else if (gameObject.tag.Contains("Magnet"))
                 {
-                    InteractWithParticleAndMagnet(gameObject);
-                    if (gameObject.tag.Contains("DataPoint"))
-                    {
-                        // Print all the data to the screen.
-                        InteractWithPointData(gameObject);
-                    }
+
+                }
+                else if (gameObject.tag.Contains("DataPoint"))
+                {
+                    InteractWithPointData(gameObject); 
                 }
             }  
         }
     }
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="gameObject"></param>
-    private void InteractWithGUI(GameObject gameObject)
-    {
-        // If the name of the gameobject contains "Dropdown", this will shift
-        if (gameObject.name.Contains("Dropdown"))
-        {
-            m_dropdown = GameObject.Find(gameObject.name).GetComponent<Dropdown>();
-            if (m_dropdown.options.Count > m_dropdown.value + 1)
-            {
-                m_dropdown.value = m_dropdown.value + 1;
-            }
-            else
-            {
-                m_dropdown.value = 0;
-            }
-        }
-        else if (gameObject.name.Contains("LoadPlotData"))
-        {
-            // GameObject.Find("Plotter").GetComponent<PointRenderer>().LoadNewPlotAxis();
-        }
-        else if (gameObject.name.Contains("Previous"))
-        {
-            Debug.Log("Previous Pressed");
-        }
-        else if (gameObject.name.Contains("LevelReset"))
-        {
-            Debug.Log("Pressed Level Reset");
-        }
-        else if (gameObject.name.Contains("IncreaseMagnetStrength"))
-        {
-            if (Int32.Parse(MagnetStrengthTextValue.text) < m_MaximumMagnetValue)
-            {
-                MagnetAttributes attributes = GameObject.Find(MagnetNameDropDown.options[MagnetNameDropDown.value].text).GetComponent<MagnetAttributes>();
-                MagnetStrengthTextValue.text = (Int32.Parse(MagnetStrengthTextValue.text) + 10).ToString();
-                attributes.MagnetismStrength = Int32.Parse(MagnetStrengthTextValue.text) / 100;
-            }
-        }
-        else if (gameObject.name.Contains("DecreaseMagnetStrength"))
-        {
-            if (Int32.Parse(MagnetStrengthTextValue.text) > m_MinimumMagnetValue)
-            {
-                MagnetAttributes attributes = GameObject.Find(MagnetNameDropDown.options[MagnetNameDropDown.value].text).GetComponent<MagnetAttributes>();
-                MagnetStrengthTextValue.text = (Int32.Parse(MagnetStrengthTextValue.text) - 10).ToString();
-                attributes.MagnetismStrength = Int32.Parse(MagnetStrengthTextValue.text) / 100;
-            }
-        }
-        else if (gameObject.name.Contains("AllowMagnetismToggle"))
-        {
-            if (gameObject.GetComponent<Toggle>().isOn == true)
-            {
-                gameObject.GetComponent<Toggle>().isOn = false;
-            }
-            else
-            {
-                gameObject.GetComponent<Toggle>().isOn = true;
-            }
-            MagnetAttributes attributes = GameObject.Find(MagnetNameDropDown.options[MagnetNameDropDown.value].text).GetComponent<MagnetAttributes>();
-            attributes.MagnetVisible = gameObject.GetComponent<Toggle>().isOn;
-        }
-        else if (gameObject.name.Contains("MagnetActiveToggle"))
-        {
-            if (gameObject.GetComponent<Toggle>().isOn == true)
-            {
-                gameObject.GetComponent<Toggle>().isOn = false;
-            }
-            else
-            {
-                gameObject.GetComponent<Toggle>().isOn = true;
-            }
-            MagnetAttributes attributes = GameObject.Find(MagnetNameDropDown.options[MagnetNameDropDown.value].text).GetComponent<MagnetAttributes>();
-            attributes.MagnetActive = gameObject.GetComponent<Toggle>().isOn;
-        }
-        else if (gameObject.name.Contains("AllowSpringToggle"))
-        {
-            if (gameObject.GetComponent<Toggle>().isOn == true)
-            {
-                gameObject.GetComponent<Toggle>().isOn = false;
-            }
-            else
-            {
-                gameObject.GetComponent<Toggle>().isOn = true;
-            }
-        }
-        else if (gameObject.name.Contains("LoadData"))
-        {
-            Debug.Log("Test");
 
-            // FileManager.OpenExplore();
-        }
-    }
     /// <summary>
     /// 
     /// </summary>
     /// <param name="gameObject"></param>
-    private void InteractWithParticleAndMagnet(GameObject gameObject)
+    private void InteractWithMagnet(GameObject gameObject)
     {
         gameObject.transform.position = transform.position;
     }
@@ -230,100 +82,73 @@ public class Raycaster : MonoBehaviour
     /// <param name="gameObject"></param>
     private void InteractWithPointData(GameObject gameObject)
     {
-        if (MagnetHolder.transform.childCount != 0 && PointHolder.transform.childCount != 0)
+        if (m_lastPointDataName != gameObject.name)
         {
-            string magnetDataMenuString = "";
-            string pointDataMenuString = "";
-            MagnetAttributes magnetAttribute;
-            pointDataMenuString = "Point Name: " + gameObject.name;
-            // Iterate through Point Data Dictionary
-            foreach (var point in gameObject.GetComponent<ParticleAttributes>().PointData)
+            gameObject.transform.localScale *= 4;
+            if (m_lastPointDataName != "")
             {
-                pointDataMenuString += "\nVariable : " + point.Key + " Value: " + point.Value;
-                magnetAttribute = MagnetHolder.transform.Find(point.Key).GetComponent<MagnetAttributes>();
-                magnetDataMenuString += "Magnet : " + point.Key +
-                                        "  Strength: " + ((point.Value - magnetAttribute.MinValue) /
-                                                        (magnetAttribute.MaxValue - magnetAttribute.MinValue)) +
-                                        "\nMin: " + magnetAttribute.MinValue +
-                                        " Max: " + magnetAttribute.MaxValue + "\n";
-
+                GameObject.Find(m_lastPointDataName).transform.localScale /= 4;
             }
-            // Fill menus
-            MagnetDataMenu.text = magnetDataMenuString;
-            PointDataMenu.text = pointDataMenuString;
+            m_lastPointDataName = gameObject.transform.name;
         }
     }
     /// <summary>
     /// 
     /// </summary>
-    private void SetXYZDropdownsToCurrentPlotAxis()
+    /// <param name="gameObject"></param>
+    private void InteractWithGUI(GameObject gameObject)
     {
-        int index = -1;
-        if (((index = GetIndexByName(XCoordinateDropDown,
-                                     GameObject.Find("X_Title").GetComponent<TextMesh>().text.ToString())) != -1))
-        { XCoordinateDropDown.value = index; }
-        if (((index = GetIndexByName(YCoordinateDropDown,
-                                     GameObject.Find("Y_Title").GetComponent<TextMesh>().text.ToString())) != -1))
-        { YCoordinateDropDown.value = index; }
-        if (((index = GetIndexByName(ZCoordinateDropDown,
-                                     GameObject.Find("Z_Title").GetComponent<TextMesh>().text.ToString())) != -1))
-        { ZCoordinateDropDown.value = index; }
-    }
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="dropDown"></param>
-    /// <param name="name"></param>
-    /// <returns></returns>
-    private int GetIndexByName(Dropdown dropDown, string name)
-    {
-        if (dropDown == null) { return -1; } // or exception
-        if (string.IsNullOrEmpty(name) == true) { return -1; }
-        List<Dropdown.OptionData> list = dropDown.options;
-        for (int i = 0; i < list.Count; i++)
-            if (list[i].text.Equals(name)) { return i; }
-        return -1;
-    }
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    private bool HasDataFileBeenUpdated()
-    {
-        if (MagnetHolder == null)
-            return false;
-        if (PointHolder == null)
-            return false;
-        // Check if the Magnet count has changed. This is an easy indicator that something has changed
-        if (m_ChildMagnetCount != MagnetHolder.transform.childCount)
+        if (gameObject.name.Contains("DropDown"))
         {
-            m_ChildMagnetCount = MagnetHolder.transform.childCount;
-            return true;
+            Dropdown dropdown = GameObject.Find(gameObject.name).GetComponent<Dropdown>();
+            if (dropdown.options.Count > dropdown.value + 1)
+            {
+                dropdown.value = dropdown.value + 1;
+            }
+            else
+            {
+                dropdown.value = 0;
+            }
         }
-        // Check file name has changed
-        if (m_FileName != "")
+        if (gameObject.name.Contains("Toggle"))
         {
-            // return true;
+            Toggle toggle = GameObject.Find(gameObject.name).GetComponent<Toggle>();
+
+            if (toggle.isOn == true)
+            {
+                toggle.isOn = false;
+            }
+            else if (toggle.isOn == false)
+            {
+                toggle.isOn = true;
+            }
+
         }
-        return false;
     }
-    /// <summary>
-    /// 
-    /// </summary>
-    private void UpdateDropDownList()
+    /*
+     * Code that I may use later. Removed
+if (MagnetHolder.transform.childCount != 0 && PointHolder.transform.childCount != 0)
+{
+    string magnetDataMenuString = "";
+    string pointDataMenuString = "";
+    MagnetAttributes magnetAttribute;
+    pointDataMenuString = "Point Name: " + gameObject.name;
+    // Iterate through Point Data Dictionary
+    foreach (var point in gameObject.GetComponent<ParticleAttributes>().PointData)
     {
-        if (m_magnetNameList.Count > 0)
-        {
-            // Clear Options
-            XCoordinateDropDown.ClearOptions();
-            YCoordinateDropDown.ClearOptions();
-            ZCoordinateDropDown.ClearOptions();
-            MagnetNameDropDown.ClearOptions();
-            // Add Options
-            XCoordinateDropDown.AddOptions(m_magnetNameList);
-            YCoordinateDropDown.AddOptions(m_magnetNameList);
-            ZCoordinateDropDown.AddOptions(m_magnetNameList);
-            MagnetNameDropDown.AddOptions(m_magnetNameList);
-        }
+        pointDataMenuString += "\nVariable : " + point.Key + " Value: " + point.Value;
+        magnetAttribute = MagnetHolder.transform.Find(point.Key).GetComponent<MagnetAttributes>();
+        magnetDataMenuString += "Magnet : " + point.Key +
+                                "  Strength: " + ((point.Value - magnetAttribute.MinValue) /
+                                                (magnetAttribute.MaxValue - magnetAttribute.MinValue)) +
+                                "\nMin: " + magnetAttribute.MinValue +
+                                " Max: " + magnetAttribute.MaxValue + "\n";
+
     }
+    // Fill menus
+    MagnetDataMenu.text = magnetDataMenuString;
+    PointDataMenu.text = pointDataMenuString;
+}*/
+
+
 }
