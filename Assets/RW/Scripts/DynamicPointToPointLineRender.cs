1433e63ecc737f182ac7428010f1f3b831c199d0
Ronald Baker <ronald_baker@brown.edu>
2019-08-02 11:06:52 -0400

    Update DynamicPointToPointLineRender.cs

--- Assets/RW/Scripts/DynamicPointToPointLineRender.cs
+++ Assets/RW/Scripts/DynamicPointToPointLineRender.cs
@@ -37,69 +37,101 @@ public class DynamicPointToPointLineRender : MonoBehaviour
 {
     // Flag indicating whether or not the spring should be viewable.
     bool m_springEnabled = true;
-    public bool SpringEnabled { get => m_springEnabled; set => m_springEnabled = value; }
+    public bool SpringEnabled { get => m_springEnabled; 
+                                set => m_springEnabled = value; }
     /// <summary>
-    /// Used to update the end points for the linerender component attached to each of the children particle point
-    /// points. This function only changes the end point values once. To achieve a real-time visual update,
-    /// this funciton should be called in some sort of update function.
+    /// Used to update the end points for the linerender component attached to 
+    /// each of the children particle point points. This function only changes 
+    /// the end point values once. To achieve a real-time visual update this 
+    /// funciton should be called in some sort of update function.
     /// </summary>
-    /// <param name="pointHolderTransform">PointHolder gameobject transform which holds all the children particle points</param>
+    /// <param name="pointHolderTransform">Parent point holder transform. Used to
+    /// access the children data points.
+    /// </param>
     public static void UpdateSpringPositions(Transform pointHolderTransform)
     {
         foreach (Transform childDataPoint in pointHolderTransform)
         {
+            // Get the line render attached to the child data point object.
             LineRenderer lineRenderer = childDataPoint.GetComponent<LineRenderer>();
-            lineRenderer.SetPosition(0, childDataPoint.GetComponent<ParticleAttributes>().OriginLocation);
+            // Set the start and end for the selected gameobject based on it's current
+            // location and the origin point.
+            lineRenderer.SetPosition(0, childDataPoint.GetComponent<ParticleAttributes>()
+                                                        .OriginLocation);
             lineRenderer.SetPosition(1, childDataPoint.transform.position);
         }
     }
     /// <summary>
-    ///  Used to set the attributes and positions for the linerender objects attached to the individual
-    ///  particle point gameobjects. This function sets the start and end point for the line render.
-    ///  Function also sets the color and width of the linerender.
+    ///  Used to set the attributes and positions for the linerender objects 
+    ///  attached to the individual data point gameobjects. This function sets 
+    ///  the start and end point for the line render.
     /// </summary>
-    /// <param name="pointHolderTransform">PointHolder gameobject transform which holds all the children particle points</param>
+    /// <param name="pointHolderTransform">Parent point holder transform. Used to
+    /// access the children data points.
+    /// </param>
     public static void GenerateLinksBetweenParticleAndOrigin(Transform pointHolderTransform)
     {
+        // Since the line render only care about the location of the game object and the 
+        // origin location. These value we selected to properly assign the number of 
+        // tracked point and their index within the linerenderer
         int positionCount = 2;
         int originIndex = 0;
         int particleIndex = 1;
+        // These values were arbitrarily 
         float startWidth = 0.002f;
         float endWidth = 0.02f;
         foreach (Transform childDataPoint in pointHolderTransform)
         {
+            // Get the line render attached to the child data point object.
             LineRenderer lineRenderer = childDataPoint.GetComponent<LineRenderer>();
             lineRenderer.positionCount = positionCount;
-            // Set the start and end widths for the spring
+            // Set the start and end widths for the spring.
             lineRenderer.startWidth = startWidth;
             lineRenderer.endWidth = endWidth;
-            // Set the start and end colors of the spring
+            // Set the start and end colors of the spring.
             lineRenderer.startColor = Color.green;
             lineRenderer.endColor = Color.red;
-            lineRenderer.SetPosition(originIndex, childDataPoint.GetComponent<ParticleAttributes>().OriginLocation);
+            // Set the start and end locations for the linerenderer. 
+            lineRenderer.SetPosition(originIndex, childDataPoint.GetComponent<ParticleAttributes>()
+                                                                .OriginLocation);
             lineRenderer.SetPosition(particleIndex, childDataPoint.transform.position);
         }
     }
     /// <summary>
-    /// Enables or diables the LineRender object in the scene.
+    /// Enables or diables the LineRender object in the scene. If true
     /// </summary>
-    /// <param name="pointHolderTransform">Transform that holds all the children particle points</param>
-    /// <param name="isEnable">Flag indicating whether or not the LineRender should be viewable in the scene.</param>
-    public static void EnableLinksBetweenParticleAndOrigin(Transform pointHolderTransform, bool isEnable)
+    /// <param name="pointHolderTransform">Parent point holder transform. Used to
+    /// access the children data points.
+    /// </param>    
+    /// <param name="isEnable">Flag indicating whether or not the LineRender 
+    /// should be viewable in the scene.
+    /// </param>
+    public static void EnableLinksBetweenParticleAndOrigin(Transform pointHolderTransform, 
+                                                           bool isEnable)
     {
         foreach (Transform childDataPoint in pointHolderTransform)
         {
+            // Iterate and set the linerender as enabled.
             childDataPoint.GetComponent<LineRenderer>().enabled = isEnable; 
         }
     }
     /// <summary>
-    ///  Used to sort data points to be added to the linerender based off accending order of the
-    ///  data points associated with a given magnet value and the selected data point.
+    ///  Used to sort data points to be added to the linerender based off accending 
+    /// order of the data points associated with a given magnet value and the 
+    /// selected data point.
     /// </summary>
-    /// <param name="PointHolderTransoform">PointHolder gameobject transform which holds all the children particle points</param>
-    /// <param name="dynamicLineRenderer">LineRender object which is currently presently attached the canvas gameobject</param>
-    /// <param name="Magnet"> The name of the magnet selected to generate the linerender for.</param>
-    public static void SortPointsForLineRender (Transform pointHolderTransoform, LineRenderer dynamicLineRenderer, string magnet)
+    /// <param name="pointHolderTransform">Parent point holder transform. Used to
+    /// access the children data points.
+    /// </param>    
+    /// <param name="dynamicLineRenderer">LineRender object which
+    /// is currently presently attached the canvas gameobject
+    /// </param>
+    /// <param name="Magnet"> The name of the magnet selected to generate the 
+    /// linerender for.
+    /// </param>
+    public static void SortPointsForLineRender (Transform pointHolderTransoform, 
+                                                LineRenderer dynamicLineRenderer, 
+                                                string magnet)
     {
         Dictionary<string, float> pointDataDictionary = new Dictionary<string, float>();
         // Get all the values from the points that correspond to the magnets.
